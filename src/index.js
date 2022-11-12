5562695ee2f5c0b42df694cec64dfa1bd79d752d
Juan Sebastian Calvo Galvis <ases741@gmail.com>
2022-11-12 09:03:15 -0500

    create middleware morgan

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+let port = 3000
+app.use(morgan('dev'))
+app.listen(port, () => {
+    console.log('listening on port ' + port)
+})
