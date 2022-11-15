f321b95310e35c293b58a12eb6675bb1c28f413d
Juan Sebastian Calvo Galvis <ases741@gmail.com>
2022-11-15 12:17:07 -0500

    connecting to postgres database
    segregation of responsibilities is used, the connection examples are created and finally a test is created to a users table previously created in postgress

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -1,6 +1,8 @@
-const {Pool} = require('pg')
-const getUsers = (req, res) => {
-    res.send('hello world in index.controllers')
+const { pool } = require('../database/postgres.connection')
+
+const getUsers = async (req, res) => {
+    const response = await pool.query('SELECT * FROM users')
+    res.status(200).json(response.rows)
 }
 
 module.exports = {getUsers}
