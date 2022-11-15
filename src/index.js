3ea9b75a526ebec1ae67618665fd6148ccc10d79
Juan Sebastian Calvo Galvis <ases741@gmail.com>
2022-11-15 10:57:51 -0500

    started create routes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const { indexMiddleware } = require('./start/index.middleware')
 const {indexSetting} =require('./start/index.setting')
 indexMiddleware(app)
 indexSetting(app)
+
 app.listen(app.get('port'), () => {
     console.log('listening on port ' + app.get('port'))
 })
