98930b70ebd24a13d9d4367f32b4a72eb9c5cebc
Juan Sebastian Calvo Galvis <ases741@gmail.com>
2022-11-15 10:23:23 -0500

    save config 15/11/2022

--- src/start/index.setting.js
+++ src/start/index.setting.js
@@ -1,6 +1,5 @@
-const express = require('express')
-const env = require('env')
-function indexSetting(app) { 
+const {config} = require('../settings/config.env')
+function indexSetting(app) {
     app.set('port',process.env.PORT)
 }
 module.exports = { indexSetting }
