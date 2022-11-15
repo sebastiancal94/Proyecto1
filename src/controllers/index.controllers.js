43523c135adb3e5d30bddc2838d9483a7c5742ba
Juan Sebastian Calvo Galvis <ases741@gmail.com>
2022-11-15 12:23:07 -0500

    updates proyects

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -1,8 +1,6 @@
 const { pool } = require('../database/postgres.connection')
-
 const getUsers = async (req, res) => {
     const response = await pool.query('SELECT * FROM users')
     res.status(200).json(response.rows)
 }
-
-module.exports = {getUsers}
+module.exports = { getUsers }
