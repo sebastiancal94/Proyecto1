4379b72c5f48256fedaa1164a1606db773ee4651
Juan Sebastian Calvo Galvis <ases741@gmail.com>
2022-11-16 12:10:02 -0500

    save changes in proyect
    CRUD mid complete, i'm not understand reason no execute the query INSERT for a create users. i need use other library to heleper connection sql, i create a new branch and use new library to create postgres connection.

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -5,24 +5,29 @@ const getUsers = async (req, res) => {
 }
 
 const getUserById = async (req, res) => {
-    const {id} = req.body
-    const response = await pool.query(`SELECT userName, email FROM users WHERE id=${id}`)
-    res.status(200).json(response.rows)
+    const id  = req.params.id
+    console.log(id)
+    const response = await pool.query(`SELECT * FROM users WHERE id = ${id}`)
+    res.status(201).json(response.rows)
 }
 const createUser = async (req, res) => {
-    const {name,userName,address_relation,geo_relation,phone,website,company} = req.body
-    const response = await pool.query(`INSERT INTO users (name,userName,address_relation,geo_relation,phone,website,company) VALUES (${name},${userName},${address_relation},${geo_relation},${phone},${website},company)`)
+    const { name, userName,email, address_relation, geo_relation, phone, website} = req.body
+    // const response = await pool.query(`INSERT INTO users (name, userName,email, address_relation, geo_relation, phone, website) VALUES (${name},${userName},${email},${address_relation},${geo_relation},${phone},${website})`)
     res.status(200).json(response.rows)
 }
 const updateUser = async (req, res) => {
-    const {name,email} = req.body
-    const response = await pool.query(`UPDATE INTO users () VALUES (${name},${email})`)
+    const id = req.params.id
+    const { name, userName, address_relation, geo_relation, phone, website, company } = req.body
+    const response = await pool.query(`UPDATE users SET name=${name}, userName=${userName}, address_relation=${address_relation}, geo_relation=${geo_relation}, phone=${phone}, website=${website}, company=${company} WHERE id=${id}`)
     res.status(200).json(response.rows)
 }
 const deleteUser = async (req, res) => {
-    const {name,email} = req.body
-    const response = await pool.query(`DELETE INTO users () VALUES (${name},${email})`)
-    res.status(200).json(response.rows)
+    const id  = req.params.id
+    console.log(id)
+    const response = await pool.query(`DELETE FROM users WHERE id = ${id}`)
+    res.status(201).json({
+        message: `User ${id} deleted successfully`
+    })
 }
 
 
