d40483a2ea8bf71a11ecbc2a66add278e8541d71
Juan Sebastian Calvo Galvis <ases741@gmail.com>
2022-11-16 12:10:02 -0500

    save changes in proyect
    CRUD mid complete, i'm not understand reason no execute the query INSERT for a create users. i need use other library to heleper connection sql, i create a new branch and use new library to create postgres connection.

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
 const router = Router()
 const {getUsers,createUser,getUserById,deleteUser,updateUser}=require('../controllers/index.controllers')
-
+router.get('/', (req, res) => {
+    res.status(201).json({ message: "hola esta es la ruta principal" })
+})
 router.get('/users', getUsers)
-router.get('/users/:userId', getUserById)
-router.get('/users/create', createUser)
-router.get('/users/:id/update', updateUser)
-router.get('/users/:id/',deleteUser)
+router.get('/users/:id', getUserById)
+router.post('/users/create', createUser)
+router.post('/users/:id', updateUser)
+router.delete('/users/:id/',deleteUser)
 module.exports = router
