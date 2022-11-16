785d5dd95eb9268404f4f8aa08e8022419d5bfbb
Juan Sebastian Calvo Galvis <ases741@gmail.com>
2022-11-16 09:47:08 -0500

    save changes in proyect
    create instruction sql for CRUD in login

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -3,4 +3,16 @@ const getUsers = async (req, res) => {
     const response = await pool.query('SELECT * FROM users')
     res.status(200).json(response.rows)
 }
-module.exports = { getUsers }
+
+const gerOneUser = async (req, res) => {
+    const {id} = req.body
+    const response = await pool.query(`SELECT userName, email FROM users WHERE id=${id}`)
+    res.status(200).json(response.rows)
+}
+const createUser = async (req, res) => {
+    const {name,email} = req.body
+    const response = await pool.query(`INSERT INTO users () VALUES (${name},${email})`)
+    res.status(200).json(response.rows)
+}
+
+module.exports = { getUsers,createUser }
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 const router = Router()
-const {getUsers}=require('../controllers/index.controllers')
+const {getUsers, createUser}=require('../controllers/index.controllers')
 
-router.get('/users',getUsers)
+router.get('/users', getUsers)
+router.post('/users2',createUser)
 module.exports = router
